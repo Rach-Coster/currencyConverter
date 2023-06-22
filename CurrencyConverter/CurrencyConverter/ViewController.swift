99f86f7b017e04a70b1e1b7e93e5fb60e385dffa
rach-coster <rachel.coster@outlook.com>
2023-06-22 15:42:31 +1000

    Added Default Project Files

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by User on 22/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
